3d83b41ab12329986378afc73f551f6324b3975f
evgeniaalejnikova <evgeniaalejnikova@MacBook-Air-Evgenia.local>
2021-12-16 17:31:06 +0300

    Test failed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -16,7 +16,7 @@ public class PostmanEchoTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(200)
+                .statusCode(201)
                 .body("data", equalTo("some data"))
         ;
     }
